e340ac1647b5a1c3fceb945848e5c6da2e14ae86
gtxaspec <gtxent@gmail.com>
2024-02-14 21:00:24 -0800

    update makefile and raptor for sensor define

--- raptor.c
+++ raptor.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-    printf("Raptor Video Daemon for %s Sensor: %s Version: %s\n", TOSTRING(SOC), SENSOR_NAME, VERSION);
+    printf("Raptor Video Daemon for %s Sensor: %s Version: %s\n", TOSTRING(SOC), TOSTRING(SENSOR), VERSION);
     IMP_System_GetCPUInfo();
     ret = system_initalize();
 	if (ret < 0) {
