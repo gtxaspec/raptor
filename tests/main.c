3f30cc94b8b38b7f7b551cf4d1ac5c65cedc0a77
Matt Davis <matteius@gmail.com>
2026-08-17 15:39:11 -0400

    fix(rwd): pace WebRTC video bursts
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- tests/main.c
+++ tests/main.c
@@ -20,6 +20,7 @@ extern SUITE(storage_suite);
 extern SUITE(timelapse_suite);
 extern SUITE(sendq_suite);
 extern SUITE(rwd_sendq_suite);
+extern SUITE(rwd_pacer_suite);
 extern SUITE(rad_clock_suite);
 extern SUITE(media_clock_suite);
 extern SUITE(resync_suite);
@@ -51,6 +52,7 @@ int main(int argc, char **argv)
 	RUN_SUITE(timelapse_suite);
 	RUN_SUITE(sendq_suite);
 	RUN_SUITE(rwd_sendq_suite);
+	RUN_SUITE(rwd_pacer_suite);
 	RUN_SUITE(rad_clock_suite);
 	RUN_SUITE(media_clock_suite);
 	RUN_SUITE(resync_suite);
