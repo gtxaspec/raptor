f751fc4e6d0f726bba1eed0c26ad6d89b9118265
Matt Davis <matteius@gmail.com>
2026-08-17 13:37:59 -0400

    test(rwd): cover send-queue recovery
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- tests/main.c
+++ tests/main.c
@@ -19,6 +19,7 @@ extern SUITE(sign_suite);
 extern SUITE(storage_suite);
 extern SUITE(timelapse_suite);
 extern SUITE(sendq_suite);
+extern SUITE(rwd_sendq_suite);
 extern SUITE(rad_clock_suite);
 extern SUITE(media_clock_suite);
 extern SUITE(resync_suite);
@@ -49,6 +50,7 @@ int main(int argc, char **argv)
 	RUN_SUITE(storage_suite);
 	RUN_SUITE(timelapse_suite);
 	RUN_SUITE(sendq_suite);
+	RUN_SUITE(rwd_sendq_suite);
 	RUN_SUITE(rad_clock_suite);
 	RUN_SUITE(media_clock_suite);
 	RUN_SUITE(resync_suite);
